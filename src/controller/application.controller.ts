ef5f446b3da2f1352900f16615c606a8a47b05ec
bharat-dangi <bharatdangi1511@gmail.com>
2024-09-05 11:38:35 +1000

    WIP: Added comments for the function create, get all, get all, delete one in application controller

--- src/controller/application.controller.ts
+++ src/controller/application.controller.ts
@@ -13,10 +13,6 @@ class ApplicationController {
   /**
    * Handles the creation of a new application.
    * It uses the ApplicationService to create an application with the data provided in the request body.
-   * @param {Request} req - The Express request object, containing the HTTP request data and application payload in the body.
-   * @param {Response} res - The Express response object, used to send the HTTP response back to the client.
-   * @param {NextFunction} next - The next middleware function in the Express stack. Used to pass control
-   * to the next middleware or handle errors.
    * @returns {Promise<void | Response>} Returns a promise that resolves to an Express response object
    * with the operation result or void if an error is handled.
    * @throws {ErrorResponse} Throws an ErrorResponse if an error occurs during application creation,
@@ -36,42 +32,91 @@ class ApplicationController {
     }
   }
 
+  /**
+   * Retrieves all applications based on the criteria provided in the request body.
+   *
+   * @static
+   * @method getAllApplications
+   * @returns {Promise<void>} A promise that resolves to sending a JSON response with the result or handling an error.
+   */
   static async getAllApplications(req: Request, res: Response, next: NextFunction) {
     try {
+      // Resolve the ApplicationService from the dependency injection container
       const applicationService: ApplicationService = container.resolve("ApplicationService") as ApplicationService;
+
+      // Extract the payload from the request body
       const payload: GetApplication = req.body;
+
+      // Call the service method to get all applications
       const { data, error } = await applicationService.getAllApplications(payload);
+
+      // If there is an error, pass it to the next middleware
       if (error) return next(new ErrorResponse(error, 500));
+
+      // Send a successful JSON response with the retrieved applications
       return res.status(200).json(buildResponse(true, data, "success"));
     } catch (error) {
+      // Log the error and pass it to the next middleware
       console.error(error);
       return next(new ErrorResponse(error, 500));
     }
   }
 
+  /**
+   * Retrieves a single application based on the criteria provided in the request body.
+   *
+   * @static
+   * @method getOneApplication
+   * @returns {Promise<void>} A promise that resolves to sending a JSON response with the result or handling an error.
+   */
   static async getOneApplication(req: Request, res: Response, next: NextFunction) {
     try {
+      // Resolve the ApplicationService from the dependency injection container
       const applicationService: ApplicationService = container.resolve("ApplicationService") as ApplicationService;
+
+      // Extract the payload from the request body
       const payloadData: GetApplication = req?.body;
 
+      // Call the service method to get one application
       const { data, error } = await applicationService.getOneApplication(payloadData);
+
+      // If there is an error, pass it to the next middleware
       if (error) return next(new ErrorResponse(error, 500));
+
+      // Send a successful JSON response with the retrieved application
       return res.status(200).json(buildResponse(true, data, "success"));
     } catch (error) {
+      // Log the error and pass it to the next middleware
       console.error(error);
       return next(new ErrorResponse(error, 500));
     }
   }
 
+  /**
+   * Deletes a single application based on the criteria provided in the request body.
+   *
+   * @static
+   * @method deleteOneApplication
+   * @returns {Promise<void>} A promise that resolves to sending a JSON response with the result or handling an error.
+   */
   static async deleteOneApplication(req: Request, res: Response, next: NextFunction) {
     try {
+      // Resolve the ApplicationService from the dependency injection container
       const applicationService: ApplicationService = container.resolve("ApplicationService") as ApplicationService;
+
+      // Extract the payload from the request body
       const payloadData: DeleteApplication = req?.body;
 
+      // Call the service method to delete one application
       const { data, error } = await applicationService.deleteOneApplication(payloadData);
+
+      // If there is an error, pass it to the next middleware
       if (error) return next(new ErrorResponse(error, 500));
+
+      // Send a successful JSON response with the result of the deletion
       return res.status(200).json(buildResponse(true, data, "success"));
     } catch (error) {
+      // Log the error and pass it to the next middleware
       console.error(error);
       return next(new ErrorResponse(error, 500));
     }
